f518860908918992387272289968e34b48cd87af
dongwook Kim <85658044+pier101@users.noreply.github.com>
2021-12-14 16:38:48 +0900

    study map

--- Python/python_basic/211213_e_study.py
+++ Python/python_basic/211213_e_study.py
@@ -36,9 +36,7 @@ print()
 a = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 a = list(map(str,list(map(lambda x : int(x)+1, a))))
 print(a)
-b = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-b = list(map(lambda x : int(x)+1, b))
-print(b)
+
 
 
 
