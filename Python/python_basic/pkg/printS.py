fed63c8dd467cef8a8ac8d574a01f703aa30457b
pier101 <85658044+pier101@users.noreply.github.com>
2022-01-28 23:59:08 +0900

    '

--- Python/python_basic/pkg/printS.py
+++
@@ -1,9 +0,0 @@
-def prt1():
-    print("I'm Niceboy")
-def prt2():
-    print("I'm Goodboy")
-    
-# 단위 실행(독립적으로 파일 실행) - 해당함수가 모듈로 써지기 전에 잘 작동 되는지 테스트하기 위함
-if __name__ == "__main__":
-    prt1()
-    prt2()
